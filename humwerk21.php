77b6ef3d6fa7e1d433e863819bfc5df19eaac537
mainol <mynolislam610@gmail.com>
2024-11-11 10:54:37 +0900

    my frist commit

---
+++ humwerk21.php
@@ -0,0 +1,12 @@
+<?php
+echo "my frist php"
+
+$txt1 ="learn PHP";
+$txt2 ="humwerk21";
+
+echo '<h2>' .$txt1.'</h2>';
+echo '<p>study frist PHP at' .$text2.'</p>';
+
+print "hello world!<br>";
+print "I'm about to learn PHP!";
+?>
