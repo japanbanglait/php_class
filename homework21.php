68713b03bee414b56bc0dfacad2901dcdb328702
mainol <mynolislam610@gmail.com>
2024-11-12 20:18:10 +0900

    homwerok dene

---
+++ homework21.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo "my frist php"
+
+$txt1 ="learn PHP";
+$txt2 ="humwerk21";
+
+echo '<h2>' .$txt1.'</h2>';
+echo '<p>study frist PHP at' .$text2.'</p>';
+
+print "hello world!<br>";
+print "I'm about to learn PHP!";
+$x = "mainol";
+echo "Hello $x";
+echo "<br>";
+$x = "mainol";
+echo 'Hello $x';
+echo "<br>";
+echo str_word_count("Hello japan!");
+echo "<br>";
+$x = "Hello japan!";
+echo strtoupper($x);
+echo "<br>";
+$x = "Hello";
+$y = "japan";
+$z = $x . " " . $y;
+echo $z;
+echo "<br>";
+echo "<h1> PHP nambers";
+$a = 5;
+$b = 5.34;
+$c = "25";
+
+var_dump($a);
+echo "<br>";
+var_dump($b);
+echo "<br>";
+var_dump($c);
+?>
+</body> 
+</html>
+
