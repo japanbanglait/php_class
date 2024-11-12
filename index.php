2d648105af4a2f138aba7c67d7d16b57643f4df0
Alom <shehabshahalom1996@gmail.com>
2024-11-12 20:11:46 +0900

    Homework

--- index.php
+++ index.php
@@ -5,4 +5,45 @@ echo $Name;
 
 
 
+
+
+
+
+$ages = 12;
+
+if($ages > 21){
+    Echo "YOu are welcome";
+}else{
+    echo "Sorry Underage NOt allow";
+} 
+
+$alo = 300;
+$peyaz = 200;
+$mas = 500;
+
+$mot = $alo + $peyaz + $mas;
+
+echo "Sorbo mot Bazzar kora holo $mot takar";
+
+
+define('Age', 'He is 20 Years old');
+
+echo Age;
+
+
+$fname ="Alom";
+$age = 27;
+$contry = "Bangladesh";
+
+echo $fname .''. $age .''. $contry;
+
+
+$name = "Mariya";
+
+echo "My NAME is A $name";
+
+echo "<h1> Hello, I am Php Developer</h1>";
+
+
+
 ?>
